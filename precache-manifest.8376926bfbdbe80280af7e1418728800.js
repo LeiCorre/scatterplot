7d9d2b86c525b003caf53c2a84846041a9577d37
jen.y.loaa@gmail.com <37940404+LeiCorre@users.noreply.github.com>
2020-07-07 18:37:07 -0600

    Updates

---
+++ precache-manifest.8376926bfbdbe80280af7e1418728800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b3b5d8ff3cda23e0486737b8339b94",
+    "url": "/scatterplot/index.html"
+  },
+  {
+    "revision": "655e01eb2f3a2bb6ca5b",
+    "url": "/scatterplot/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "5d7983b3d0230571308f",
+    "url": "/scatterplot/static/js/2.05407029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scatterplot/static/js/2.05407029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655e01eb2f3a2bb6ca5b",
+    "url": "/scatterplot/static/js/main.989ce21c.chunk.js"
+  },
+  {
+    "revision": "f64fb66d5c49a5ccbbf8",
+    "url": "/scatterplot/static/js/runtime-main.332fce24.js"
+  }
+]);
